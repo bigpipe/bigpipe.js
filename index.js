6bf0b6106977599deba340d5f68d9e7e96b6317c
Swaagie <info@martijnswaagman.nl>
2015-03-12 17:34:17 +0100

    [fix] remove double slash

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ Fittings.extend({
   // The template that is flushed to the client every single time a pagelet is
   // ready to write it's output.
   //
-  fragment: read(join(__dirname, '/fragment.html'), 'utf-8').split('\n').join(''),
+  fragment: read(join(__dirname, 'fragment.html'), 'utf-8').split('\n').join(''),
 
   //
   // Library bootstrap which will be passed into the bootstrap pagelet.
   //
-  bootstrap: read(join(__dirname, '/bootstrap.html'), 'utf-8'),
+  bootstrap: read(join(__dirname, 'bootstrap.html'), 'utf-8'),
 
   //
   // Wrapping template to introduce client-side templates.
   //
-  template: read(join(__dirname, '/template.js'), 'utf-8'),
+  template: read(join(__dirname, 'template.js'), 'utf-8'),
 
   //
   // Wrapping template to introduce plugin client code.
   //
-  plugin: read(join(__dirname, '/plugin.js'), 'utf-8'),
+  plugin: read(join(__dirname, 'plugin.js'), 'utf-8'),
 
   //
   // Reference(s) to the library files that should be loaded.
